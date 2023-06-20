2a7fab93d833a73e7fd587431bd981a2f78b3ce5
Ravi Kumar <kumar.ravi.vegan@gmail.com>
2023-06-21 04:03:30 +0530

    update the hero section with the 3d computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,5 +1,5 @@
-import { Suspense, useEffect, useState } from 'react'
-import { Canvas } from '@react-three/fiber'
+import React, { Suspense, useEffect, useState } from 'react'
+import { Canvas, extend } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
 import CanvasLoader from '../Loader'
@@ -8,8 +8,16 @@ const Computers = () => {
     const computer = useGLTF('./desktop_pc/scene.gltf')
     return (
         <mesh>
-            <hemisphereLight intensity={0.15} groundColor="black" />
+            <hemisphereLight intensity={0.15} groundcolor="black" />
             <pointLight intensity={1} />
+            <spotLight
+                position={[-20, 50, 10]}
+                angle={0.12}
+                penumbra={1}
+                intensity={1}
+                castShadow
+                shadow-mapSize={1024}
+            />
             <primitive
                 object={computer.scene}
                 scale={0.75}
